c68948a6b9053b47e4cef1e15d6cafac362c5f87
YamiletRetana <retanayamilet6@gmail.com>
2021-06-26 17:01:34 -0500

    actualizar

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const model = require('../model/valores')();//modelo con referencia
-
 const Valor = require('../model/valores');//constante que extrae el valor solicitado de valores
 
-//req es lo que estamos solicitando al servidor y lo que nos estaria contestando
-router.get('/', async (req, res ) => {
-    const valores = await Valor.find();//extrae todos los valores que esten dentro de la BD
-    console.log(valores);//imprimir los valores
-    res.render('index.ejs', {
+//formato para fisualizar el formulario
+router.get('/', async (req, res) =>{
+    const valores = await Valor.find();
+    console.log(valores);
+    res.render('index.ejs',{
         valores
     });
-//res.send('Hola IOT')
 });
-
-router.post('/add', async (req, res ) => {
+router.post('/add', async (req, res) =>{
     const valor = new Valor(req.body);
     await valor.save();
-    res.redirect('/');//redirrecionar ruta
+    res.redirect('/');  
+});
+
+//formato con metod get y post pero con json
+router.get('/', async (req, res) =>{
+    const valores = await Valor.find();
+    console.log(valores);
+    res.json(valores);
+    /* res.render('index.ejs',{
+        valores
+    });*/  
+});
+router.post('/add', async (req, res) =>{
+    const valor = new Valor(req.body);
+    await valor.save(); 
+    res.json(req.body);
 });
 
 module.exports = router;
